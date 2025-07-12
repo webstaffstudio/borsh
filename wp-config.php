6fffa1b4d90e847528eb8bcef646e6e21cd31a02
serhii.trytenko <serhii.trytenko@beetroot.se>
2025-07-12 12:52:18 +0300

    update: gitignore

--- wp-config.php
+++ wp-config.php
@@ -88,8 +88,8 @@ $table_prefix = 'wp_';
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 if ( ! defined( 'WP_DEBUG' ) ) {
-	define( 'WP_DEBUG', false );
-	define( 'WP_DEBUG_LOG', false );
+	define( 'WP_DEBUG', true );
+	define( 'WP_DEBUG_LOG', true );
 }
 
 define( 'WP_ENVIRONMENT_TYPE', 'local' );
