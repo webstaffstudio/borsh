9dae564351c7a42cc2a84c1217667fa8c81fcd22
olekspiven <alex.piven@bilberrry.com>
2025-07-12 12:11:33 +0300

    theme configuration

--- wp-content/themes/borsh/includes/js-css.php
+++ wp-content/themes/borsh/includes/js-css.php
@@ -27,8 +27,6 @@ function _add_stylesheets() {
     wp_dequeue_style('wc-block-style');
     wp_dequeue_style('global-styles');
     wp_dequeue_style('classic-theme-styles');
-    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css');
-    wp_enqueue_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
 	wp_enqueue_style('theme', get_template_directory_uri() . '/assets/dist/css/main.css', null, null, 'all' );
 }
 add_action('wp_enqueue_scripts', '_add_stylesheets');
--- wp-content/themes/borsh/template-parts/blocks/welcome.php
+++ wp-content/themes/borsh/template-parts/blocks/welcome.php
@@ -6,3 +6,13 @@
  * Keywords: welcome, intro, section
  * Supports: {"align":true,"mode":false,"multiple":true}
  */
+?>
+<section class="hero">
+    <div class="container">
+        <div class="hero__wrapper">
+            <div class="hero__title">
+
+            </div>
+        </div>
+    </div>
+</section>
