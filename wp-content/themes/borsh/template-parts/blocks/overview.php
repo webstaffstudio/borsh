dc778aec84401ca6d890f9ababb6f38e49415698
olekspiven <alex.piven@bilberrry.com>
2025-07-13 00:04:23 +0300

    overview section

---
+++ wp-content/themes/borsh/template-parts/blocks/overview.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Block Name: Огляд Події
+ * Description:
+ * Icon: admin-users
+ * Keywords: welcome, intro, section
+ * Supports: {"align":true,"mode":false,"multiple":true}
+ */
+?>
+<section class="overview">
+    <div class="container">
+        <div class="overview__about">
+            <div class="col">
+                <div class="overview_top_logo"></div>
+                <div class="b-title"></div>
+                <div class="overview_description"></div>
+            </div>
+            <div class="col">
+                <h3 class="text-medium"></h3>
+                <div class="overview__image"></div>
+            </div>
+        </div>
+        <div class="overview__counters">
+
+        </div>
+        <div class="overview__footer"></div>
+    </div>
+</section>
