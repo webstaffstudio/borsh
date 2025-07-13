aa684d90a219c0e9959aa85d3a49d75380b37c4a
serhii.trytenko <serhii.trytenko@beetroot.se>
2025-07-13 16:56:52 +0300

    add: poster (finish)

---
+++ wp-content/themes/borsh/template-parts/blocks/poster.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Block Name: Постер
+ * Description: A customizable block for displaying welcome slider section with a background, title, description, and optional button.
+ * Icon: admin-users
+ * Keywords: welcome, intro, section
+ * Supports: {"align":true,"mode":false,"multiple":true}
+ */
+
+
+$img = get_field('image');
+$src_img = $img['sizes']['large'];
+$src_filter = get_stylesheet_directory_uri() . '/assets/src/img/bg-poster.svg';
+$style = !$img['url'] ? 'style="height: auto"' : '';
+$align = !$img['url'] ? 'style="display:flex;justify-content: center;text-align:center"' : '';
+$text = get_field('text');
+
+if ($text || $img) : ?>
+    <section class="poster bg-chamois d-flex ai-end" <?= $style; ?>>
+        <?php if ($img['url']) : ?>
+            <div class="poster__img"><img class="w-100" src="<?= $src_img; ?>" alt="title"></div>
+
+            <div class="poster__filter"><img class="w-100" src="<?= $src_filter; ?>" alt="title"></div>
+        <?php endif; ?>
+
+        <?php if ($text) : ?>
+            <div class="poster__container container py-40" <?= $align; ?>>
+                <div class="poster__wrapper mw-565 w-100">
+                    <h3 class="text-medium text-upper color-blue fs-36@ls fs-26 fw-400 m-0"><?= $text; ?></h3>
+                </div>
+            </div>
+        <?php endif; ?>
+    </section>
+<?php endif; ?>
